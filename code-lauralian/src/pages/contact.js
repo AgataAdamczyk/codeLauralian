25c41a150fa9777e61418a76969f9183c51d814d
AgataAdamczyk <agata.bodakiewicz@wp.pl>
2020-10-25 22:16:18 +0100

    Add contact form

--- code-lauralian/src/pages/contact.js
+++ code-lauralian/src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Formik } from 'formik';
+import Button from 'components/Button/Button';
 
 const PageWrapper = styled.div`
   width: 100vw;
@@ -26,11 +28,90 @@ const Decor = styled.span`
   margin: 10px;
 `;
 
+const StyledInput = styled.input`
+  display: block;
+  border: 2px solid black;
+  background: none;
+  font-size: 20px;
+  height: ${({as}) => as ? '200px' : 'auto'};
+  width: ${({as}) => as ? '500px' : '300px'};
+  margin-bottom: ${({as}) => as && '40px'};
+`;
+
+const StyledLabel = styled.label`
+  margin: 30px 0 10px;
+  display: block;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
 const ContactPage = () => (
   <PageWrapper>
     <Title>Kontakt</Title>
     <Decor>. . .</Decor>
     <Paragraph>Jak mogę Ci pomóc ?</Paragraph>
+    <Formik
+       initialValues={{ name: '', email: '', message: '' }}
+      //  validate={values => {
+      //    const errors = {};
+      //    if (!values.email) {
+      //      errors.email = 'Required';
+      //    } else if (
+      //      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      //    ) {
+      //      errors.email = 'Invalid email address';
+      //    }
+      //    return errors;
+      //  }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           console.log(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+        //  errors,
+        //  touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+       }) => (
+        <form onSubmit={handleSubmit}>
+          <StyledLabel htmlFor="name">Imię</StyledLabel>
+          <StyledInput
+            id="name" 
+            type="text" 
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name} 
+          />
+          <StyledLabel htmlFor="e-mail">E-mail</StyledLabel>
+          <StyledInput
+            id="email" 
+            type="e-mail" 
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email} 
+          />
+          <StyledLabel htmlFor="name">Wiadomość</StyledLabel>
+          <StyledInput 
+            as="textarea"
+            id="message"
+            type="text" 
+            name="message"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email} 
+          />
+          <Button type="submit " disabled={isSubmitting}>Wyślij</Button>
+         </form>
+       )}
+     </Formik>
   </PageWrapper>
 );
 
--- code-lauralian/src/pages/index.js
+++ code-lauralian/src/pages/index.js
@@ -6,44 +6,31 @@ import Button from 'components/Button/Button';
 import PropTypes from 'prop-types';
 
 const ContentWrapper = styled.div`
-  padding: 50px 0;
-  width: 50%;
-  position: relative;
-  text-align: right;
+  display: flex;
   flex-direction: column;
-  /* justify-content: center; */
-  align-items: flex-end; 
-
-  h1 {
-    font-size: 30px;
-    /* margin: 0;
-    width: 40%; */
-    line-height: 0.98;
-  }
-
-  p {
-    margin: 20px 0 40px;
-    /* width: 40%;  */
-  }
+  padding: 40px;
+  align-content: center;
 `;
 
-const StyledImage = styled(Image)`
-  position: absolute; 
-  top: 60;
-  right: 60;
-  /* width: 35%; */
-  /* height: 90vh; */
-  /* object-fit: cover; */
+const Title = styled.h1`
+  text-align: right;
 `;
+
+const Paragraph = styled.p`
+  text-align: right;
+  line-height: 1.8;
+`;
+
+const StyledImage = styled(Image)``;
 
 const IndexPage = ({ data }) => (
   <>
     <ContentWrapper>
-      <h1>Cześć, mam na imię Agata!</h1>
-      <p>Jestem Junior Front-end Developerem</p>
+      <Title>Cześć, mam na imię Agata!</Title>
+      <Paragraph>Jestem Junior Front-end Developerem</Paragraph>
       <Button as={Link} to='/portfolio'>Moje portfolio</Button>
+      <StyledImage fixed={data.file.childImageSharp.fixed} />
     </ContentWrapper>
-    <StyledImage fixed={data.file.childImageSharp.fixed} />
   </>
 );
 
